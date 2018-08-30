4d16ab389bd3c0adbb58d4d671c5031fede97937
Krunal vaghasiya <vaghasiyakrunal06@gmail.com>
2018-08-30 17:39:22 +0530

    travis config

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://krunalmlab:mlab1990@ds125402.mlab.com:25402/blog_devs',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
